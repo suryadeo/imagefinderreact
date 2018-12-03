7ffb3b634ee59535a9114d2979ebca3086643295
suryadeo <surya.d.sindhunata@gmail.com>
2018-12-03 14:47:57 +0700

    new push

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import NavBar from './Components/navbar/NavBar';
+import Search from './Components/search/Search';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div>
+            <NavBar />
+            <Search/>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/search/Search.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react';
+import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+import axios from 'axios';  
+import ImageResults from '../imageresults/ImageResults';
+
+class Search extends Component{
+    state = {
+        searchText: '',
+        amount: 15,
+        apiUrl:'https://pixabay.com/api/',
+        apiKey:'10876328-ec15fe3a536ef8b23cb11b0f8',
+        images:[]
+    }
+
+    onTextChange = (e) => {
+        const val=e.target.value;
+        this.setState({[e.target.name]: e.target.value }, () => {
+            if(val === ''){
+                this.setState({images: []});
+            }else{
+                axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
+                    this.state.searchText
+                }&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+            )
+                .then(res=> this.setState({images:res.data.hits}))
+                .catch(err => console.log(err));
+            }
+        });
+    };
+    onAmountChange=(e, index, value)=> this.setState({amount: value})
+
+    render() {
+        console.log(this.state);
+        return (
+            <div>
+                <TextField
+                    name="searchText"
+                    value={this.state.searchText}
+                    onChange={this.onTextChange}
+                    floatingLabelText="Search Image"
+                    fullwidth='true' />
+                <br />
+                <SelectField
+                    name="amount"
+                    floatingLabelText="Amount"
+                    value={this.state.amount}
+                    onChange={this.onAmountChange}
+                >
+                    <MenuItem value={5} primaryText="5" />
+                    <MenuItem value={10} primaryText="10" />
+                    <MenuItem value={15} primaryText="15" />
+                    <MenuItem value={30} primaryText="30" />
+                    <MenuItem value={50} primaryText="50" />
+                </SelectField>
+                <br />
+        {this.state.images.length > 0 ? ( <ImageResults images={this.state.images}/>) : null}
+            </div>
+        )
+    }
+}
+
+export default Search;
